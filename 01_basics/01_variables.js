7c48a57e5c880bbd1cbdbc8039807e402add5fdc
shaleen69 <143455272+shaleen69@users.noreply.github.com>
2024-07-09 14:57:32 +0000

    01_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId=1232
+let accountEmail="kumar123@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+
+accountEmail="1ds21ec103@gmail.com"
+accountPassword="2121"
+accountCity="Bengaluru"
+
+console.log(accountId);
+/*
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);_basics
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity]);
