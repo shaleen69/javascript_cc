65deafff198bb9970ed58f6ffbd1cd644bfaffcb
shaleen69 <143455272+shaleen69@users.noreply.github.com>
2024-07-10 13:05:04 +0000

    conversion.js

---
+++ 01_basics/02_conversion.js
@@ -0,0 +1,28 @@
+let score=true
+console.log(typeof score);
+
+let v="33"
+console.log(typeof v);
+
+let g=Number(score);
+console.log(typeof g);
+console.log(g);
+
+/*
+ "33" =>33
+ "33abc" =>NaN
+ true=>1;false=>0
+*/
+
+let islogg="shaleen";
+let vool=Boolean(islogg);
+console.log(typeof islogg);
+console.log(typeof vool);
+console.log(vool);
+
+/*
+  1=>true;
+  0=>false;
+  ""=>false;
+  "shaleen"=>true;
+*/
