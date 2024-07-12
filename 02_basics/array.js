54dd3696981d069968f8fd3cc574c3409e79c61c
shaleen69 <143455272+shaleen69@users.noreply.github.com>
2024-07-12 15:03:39 +0000

    arrayjs

--- 02_basics/array.js
+++ 02_basics/array.js
@@ -7,3 +7,16 @@ arr1.splice(1,3)
 console.log(arr1);
 
 //in slice copy is made while in splice reference is passed so any chages in original array wil reflect changes
+
+const marvel_he=["thor","ironman","spiderman"]
+const dc_he=["superman","flash","batman"]
+marvel_he.push(dc_he)
+console.log(marvel_he);
+
+const another_array=[1,2,3,[4,5,6],7,6,7,[4,5]]
+console.log(another_array);
+
+const real_array=another_array.flat(Infinity)
+console.log(real_array);
+console.log(Array.isArray("Shaleen"));
+console.log(Array.from("Shaleen"));
