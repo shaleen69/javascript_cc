a98a4da0729ab0257672b903fc0ea3cde8cd702d
shaleen69 <143455272+shaleen69@users.noreply.github.com>
2024-07-15 14:23:28 +0000

    json&api

---
+++ 02_basics/json.js
@@ -0,0 +1,17 @@
+const course={
+    coursename:"js in hindi",
+    price:"999",
+    courseinst:"shaleen"
+}
+console.log(course.courseinst);
+const{courseinst}=course
+console.log(courseinst);
+
+//objects
+const obj={
+
+}
+//json 
+{
+    //not a object it is json which is widely used in api for as objectname is not defined
+}
