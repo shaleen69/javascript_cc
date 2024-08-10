5f4eb1f489a4e79e99883e03bfaccbf283289568
shaleen69 <143455272+shaleen69@users.noreply.github.com>
2024-08-10 08:00:24 +0000

    scope

---
+++ 03_basics/scope.js
@@ -0,0 +1,11 @@
+console.log(addone(5))
+function addone(num)
+{
+    return num+1
+}
+
+const addtwo=function(num)
+{
+    return num+2;
+}
+console.log(addtwo(5))//here u are storing function value in variable addtwo so u cant write console.log(addtwo)above it as it cant print variable without initializing .
